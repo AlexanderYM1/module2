6f9a164dbfdc211ef8df33a61e967cb466590208
AlexanderYM1 <malinaeef@icloud.com>
2024-10-13 22:49:50 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,12 @@
+number = int(input('Введите число от 3 до 20:'))
+if number < 3 or number> 20:
+    print('Ошибка!!! Введенное число',number,'не подходит условиям' )
+else:
+    result= ''
+    list = range(1,21)
+    for i in range(len(list)):
+        for j in range(i+1, len(list)):
+            pair_sum = list[i] + list[j]
+            if number % pair_sum == 0:
+                result = result + str(list[i]) + str(list[j])
+    print(result)
